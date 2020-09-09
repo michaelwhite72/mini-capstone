5373305dc39cb9901836dee30860cf705435f072
Mike <mwhite7372@gmail.com>
2020-09-09 12:04:22 -0500

    Added and tested JSON route for all available products

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,2 +1,10 @@
 class Api::ProductsController < ApplicationController
+
+  def all_products_action
+    @all_products = Product.all
+    render "all_products.json.jb"
+    
+  end 
+
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+my_product = Product.new({name: "fan", price: 10, description: "cooling device"})
+my_product.save
+
+my_product = Product.new({name: "Laptop", price: 900, description: "Portable computer"})
+my_product.save
+
+my_product = Product.new({name: "camera", price: 125, description: "Takes pitcures of important moments"})
+my_product.save
