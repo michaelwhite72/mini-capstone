61fa7536ca4661bedc4993ae1a080aca3fdeb967
Mike <mwhite7372@gmail.com>
2020-09-09 12:40:28 -0500

    Added JSON route for each individual product

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,17 @@ Rails.application.routes.draw do
   namespace :api do
     get "/all_products_path" => "products#all_products_action"
   end
+
+  namespace :api do
+    get "/camera_path" => "products#camera_action"
+  end
+
+  namespace :api do
+    get "/fan_path" => "products#fan_action"
+  end
+
+  namespace :api do
+    get "/laptop_path" => "products#laptop_action"
+  end
+
 end
